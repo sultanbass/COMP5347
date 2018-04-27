742652d9c6c4daf6f4f60f2391419251df30c36f
Bass <Bass@DESKTOP-4GEFFB6>
2018-04-27 22:44:17 +1000

    Changed var to const.

--- server.js
+++ server.js
@@ -2,14 +2,14 @@
  * Assigment 2 - main server
  */
 
-var express = require('express')
+const express = require('express')
 var path = require('path')
 var bodyParser = require('body-parser');
 var session = require('express-session');
 
 var serversession = require('./app/routes/server.routes');
 
-var server = express();
+const app = express();
 
 /* variables here 
 * 
@@ -18,12 +18,12 @@ var server = express();
 *
 */
 
-server.set('views', path.join(__dirname, 'app/views'));
-server.use(express.static(path.join(__dirname, 'public')));
-server.use(bodyParser.json())
-server.use(bodyParser.urlencoded({ extended: false }))
-server.use(session({secret: 'ssshhhh', cookie:{maxAge:600000}, resave: true, saveUninitialized: true}));
-server.use('/', serversession);
+app.set('views', path.join(__dirname, 'app/views'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(session({secret: 'ssshhhh', cookie:{maxAge:600000}, resave: true, saveUninitialized: true}));
+app.use('/', serversession);
 
 //route methods moved to server.routes
 //app.get('/', function(req,res){
@@ -35,6 +35,6 @@ server.use('/', serversession);
 //	res.render('surveyresult.ejs', {products: products, surveyresults: surveyresults})
 //});
 
-server.listen(3000, function () {
+app.listen(3000, function () {
   console.log('survey app listening on port 3000!')
 })
