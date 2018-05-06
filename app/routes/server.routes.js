d69e2f07873264a1fb4cb1610bf4a18c2c30fb59
Burhan <burhanrajbhoy@gmail.com>
2018-05-06 12:51:04 +1000

    commiting all changes to resolve dependecies.

--- app/routes/server.routes.js
+++ app/routes/server.routes.js
@@ -1,17 +1,74 @@
 var express = require('express');
 var controller = require('../controllers/server.controller');
 var router = express.Router();
+const bcrypt = require('bcryptjs');
+const passport = require('passport');
 
-//declare the varaible to hold the user model
+//Bring the user model and wiki articles model
 var User = require('../models/users');
+var Wiki = require('../models/wikiarticles');
+
 
 // Get request routes
 router.get('/', controller.showForm);
 router.get('/register', controller.register);
 
+
 // Post request routes
 router.post('/userdashboard', controller.userdashboard);
 
+
+//registration process
+router.post('/register', function(req, res){
+  var first_name = req.body.firstname;
+  var last_name = req.body.lastname;
+  var email = req.body.email;
+  var username = req.body.username;
+  var password = req.body.password;
+
+  //validate the fields
+  req.checkBody('name', 'Name is required').notEmpty();
+  req.checkBody('email', 'Email is required').notEmpty();
+  req.checkBody('email', 'Email is not valid').isEmail();
+  req.checkBody('username', 'Username is required').notEmpty();
+  req.checkBody('password', 'Password is required').notEmpty();
+
+  //check if there are any validation errors
+  var errors = req.validationErrors();
+  if(errors){
+    res.render('register', {
+      errors:errors
+    });
+  } else {
+    let newUser = new User({
+      first_name:firstname,
+      last_name:lastname,
+      email:email,
+      username:username,
+      password:password
+    });
+
+    //encrypt the password with hash
+    bcrypt.genSalt(10, function(err, salt){
+      bcrypt.hash(newUser.password, salt, function(err, hash){
+        if(err){
+          console.log(err);
+        }
+        newUser.password = hash;
+        //save new user
+        newUser.save(function(err){
+          if(err){
+            console.log(err);
+            return;
+          } else {
+            req.flash('success','Registration successful. You can now log in.');
+            res.redirect('/');
+          }
+        });
+      });
+    });
+  }
+});
 
 // Allows other files to include the router
 module.exports = router;
--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const expressValidator = require('express-validator');
 const app = express();
 const flash = require('connect-flash');
 
-// This is a method used to set environment variables that Express will use in its configuration 
+// This is a method used to set environment variables that Express will use in its configuration
 app.set('views', path.join(__dirname, 'app/views'));
 app.set('view engine', 'pug');
 //This is the path for the static folder
@@ -62,7 +62,6 @@ app.use('/', serversession);
 //Connect to mongodb
 mongoose.connect('mongodb://localhost/wikipedia');
 var db = mongoose.connection;
-
 //check DB connection
 db.once('open', function (){
   console.log('Connected to MongoDB');
