3aab5ff9fe873fc1838b09f1ba359dca92a6013c
Burhan <burhanrajbhoy@gmail.com>
2018-05-06 11:43:54 +1000

    updated server.js file to include middleware for express-validator, connect flash

--- server.js
+++ server.js
@@ -9,11 +9,13 @@ var bodyParser = require('body-parser');
 var session = require('express-session');
 var serversession = require('./app/routes/server.routes');
 var register = require('./app/routes/users');
+const expressValidator = require('express-validator');
 const app = express();
+const flash = require('connect-flash');
 
 // This is a method used to set environment variables that Express will use in its configuration
 app.set('views', path.join(__dirname, 'app/views'));
-
+app.set('view engine', 'pug');
 //This is the path for the static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -24,6 +26,31 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // Register passport middleware
 app.use(passport.initialize());
 app.use(passport.session());
+
+// Express Messages Middleware
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
+
+// Express Validator Middleware
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
 
 // code to maintain a user session
 app.use(session({secret: 'ssshhhh', cookie:{maxAge:600000}, resave: true, saveUninitialized: true}));
