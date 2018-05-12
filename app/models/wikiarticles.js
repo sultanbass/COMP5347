05383cce248ae69d51265bdbb721d92517f90bbc
Burhan <burhanrajbhoy@gmail.com>
2018-05-12 14:28:06 +1000

    added test function

--- app/models/wikiarticles.js
+++ app/models/wikiarticles.js
@@ -1,22 +1,30 @@
 const mongoose = require('mongoose');
 
-//Create schema for wiki pages  
+//Create schema for wiki pages
 const wikiSchema = new mongoose.Schema ({
   title:{
     type: String,
+    index: true,
   },
   timestamp:{
     type: String,
+    index: true,
   },
   user:{
     type: String,
+    index: true,
   },
   anon:{
      type: String,
+     index: true,
   },
 });
 
-
+//Get names of all wikiarticles
+wikiSchema.revisions.Titlename = function(callback){
+	return this.distinct("title")
+	.exec(callback)
+};
 //var Wiki = mongoose.model('Wiki',wikiSchema, 'wikipedia')
 
 //Export the model wiki Schema
